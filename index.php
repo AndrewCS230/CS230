72fdf064516f47fe90d502de20697a656d0b0983
AndrewCS230 <agl0078@mix.wvu.edu>
2021-02-05 20:04:52 -0500

    Lab2 about page

---
+++ index.php
@@ -0,0 +1,50 @@
+<body>
+    <link rel="stylesheet" href="css/about.css">
+
+    <div class="about">
+        <h1>About Us</h1>
+        <p>My CS 230 Lab website built for the purpose reviewing things I think</p>
+    </div>
+    <div class="row">
+        <div class="column">
+            <div class="card">
+                <img src="images/1.jpeg">
+                <div class="container">
+                    <h2>Dictator</h2>
+                    <p>Danny DeVito</p>
+                    <p>Email: trashman@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/2.jpeg">
+                <div class="container">
+                    <h2>Commander</h2>
+                    <p>Thanos</p>
+                    <p>Email: inevitable@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/3.jpeg">
+                <div class="container">
+                    <h2>Captain</h2>
+                    <p>Samuel L. Jackson</p>
+                    <p>Email: what@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="card">
+                <img src="images/4.jpeg">
+                <div class="container">
+                    <h2>Treasurer</h2>
+                    <p>Abraham Lincoln</p>
+                    <p>Email: Abe@gmail.com</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
