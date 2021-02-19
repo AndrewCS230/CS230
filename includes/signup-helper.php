a7fe6bf4186febcffe6d8b3e82fe3524766833b2
AndrewCS230 <alg0078@mix.wvu.edu>
2021-02-18 19:03:17 -0500

    debug

--- includes/signup-helper.php
+++ includes/signup-helper.php
@@ -27,14 +27,14 @@ if(isset($_POST['signup-submit'])){
             mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
-            $check = mysqli_stmt_nun_rows($stmt);
+            $check = mysqli_stmt_num_rows($stmt);
 
-            if(check>0){
+            if($check > 0){
                 header("Location:../signup.php?error=UsernameTaken");
             exit();
             }
             else{
-                $sql = "INSERT INTO users (lname, fname, email, uname, password) VALUES (?, ?, ?, ?)";
+                $sql = "INSERT INTO users (lname, fname, email, uname, password) VALUES (?, ?, ?, ?, ?)";
                 $stmt = mysqli_stmt_init($conn);
                 if(!mysqli_stmt_prepare($stmt, $sql)){
                     header("Location:../signup.php?error=SQLInjection");
